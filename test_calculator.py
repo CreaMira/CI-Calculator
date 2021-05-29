745f935aa6362ef06b502ecdd4f1ba63053c43ad
Nuocheng Shen <shennu@oregonstate.edu>
2021-05-29 13:12:17 -0700

    formating...
    ./calculator.py:1:9: E201 whitespace after '('
    ./calculator.py:1:11: E203 whitespace before ','
    ./calculator.py:1:15: E202 whitespace before ')'
    ./calculator.py:4:1: E302 expected 2 blank lines, found 1
    ./calculator.py:4:14: E201 whitespace after '('
    ./calculator.py:4:16: E203 whitespace before ','
    ./calculator.py:4:20: E202 whitespace before ')'
    ./calculator.py:7:1: W391 blank line at end of file
    ./test_calculator.py:3:1: E302 expected 2 blank lines, found 1
    ./test_calculator.py:5:37: E231 missing whitespace after ','
    ./test_calculator.py:8:42: E231 missing whitespace after ','
    ./test_calculator.py:10:1: W391 blank line at end of file
    2     E201 whitespace after '('
    2     E202 whitespace before ')'
    2     E203 whitespace before ','
    2     E231 missing whitespace after ','
    2     E302 expected 2 blank lines, found 1
    2     W391 blank line at end of file

--- test_calculator.py
+++ test_calculator.py
@@ -1,10 +1,13 @@
+
+
 import calculator
+
 
 class TestCalculatorApp:
     def test_add(self):
-        assert 5 == calculator.add(3,2)
+        assert 5 == calculator.add(3, 2)
 
     def test_substract(self):
-        assert 1 == calculator.subtract(3,2)
+        assert 1 == calculator.subtract(3, 2)
 
 
